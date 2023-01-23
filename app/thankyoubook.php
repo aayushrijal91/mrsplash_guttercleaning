b20294aa162b224225e11ad480b4432780596644
Aayush Rijal <arijal@aiims.com.au>
2023-01-23 17:02:36 +1100

    Tracking installed

--- app/thankyoubook.php
+++ app/thankyoubook.php
@@ -1,13 +1,15 @@
 <?php include __DIR__ . '/functions.php'; ?>
 
 <?php include __DIR__ . '/src/header.php'; ?>
-<!-- Event snippet for Quote Form conversion page -->
+<!-- Event snippet for Booking Form conversion page -->
 
 <script>
     gtag('event', 'conversion', {
-        'send_to': 'AW-11076019736/Ex2dCMDGgYkYEJjMuqEp'
+        'send_to': 'AW-11076019736/DGUyCMPGgYkYEJjMuqEp'
     });
 </script>
+
+
 <div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
     Thank you! Your message has been sent. We will contact you shortly.
 </div>
